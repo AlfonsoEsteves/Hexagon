553c988abace63e3cbf51c92c0dbdf19e7874a7f
alf <alf@alf.alf>
2019-07-28 22:39:41 -0300

    -

--- Hexagon/src/game/Rnd.java
+++ Hexagon/src/game/Rnd.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Rnd {
 
-    public static Random rnd = new Random(44);
+    public static Random rnd = new Random(45);
 
     public static int nextInt(int x){
         return rnd.nextInt(x);
