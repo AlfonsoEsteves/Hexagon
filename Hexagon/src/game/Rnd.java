f50f5924eb11d4fb0f621cca8eaa9f86cc7c4fec
alf <alf@alf.alf>
2019-11-16 20:12:07 -0300

    -

--- Hexagon/src/game/Rnd.java
+++ Hexagon/src/game/Rnd.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Rnd {
 
-    public static Random rnd = new Random(55);
+    public static Random rnd = new Random(56);
 
     public static int nextInt(int x){
         return rnd.nextInt(x);
--- Hexagon/src/game/unit/demon/Demon.java
+++ Hexagon/src/game/unit/demon/Demon.java
@@ -14,7 +14,7 @@ public class Demon extends Unit {
 
     public Demon(int x, int y) {
         super(x, y);
-        life = 125;
+        life = 120;
         tasks.add(TaskHuntPerson.instance);
         tasks.add(TaskDestroy.instance);
     }
